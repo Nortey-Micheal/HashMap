e8a064295142ef4a40dd08965d577c98a628f748
Nortey-Micheal <norteymicheal91@gmail.com>
2024-08-31 08:09:53 -0700

    "Added LinkedList implementation and modified HashMap to include Set method and use LinkedList for collision resolution"

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+function Node (value = null,next = null) {
+    this.value = value;
+    this.nextNode = next;
+}
+
+const LinkedList = function () {
+    this.value = 'head';
+    this.nextNode = null;
+    const append = (value) => {
+        if (this.nextNode === null) return this.nextNode = new Node(value);
+        else {
+            let temp = this.nextNode
+            while (temp.nextNode != null) temp = temp.nextNode;
+            temp.nextNode = new Node(value);
+        }
+
+    }
+
+    return {
+        append
+    }
+}
+
 function HashMap () {
+    let Bucket = [];
     const Hash = (key) => {
         let hashCode = 0;
       
@@ -10,11 +34,37 @@ function HashMap () {
         return hashCode;
     }
 
+    const Set = (key,value) => {
+        let hashCode = Hash(key);
+
+        // Bucket[hashCode].key = key;
+        // Bucket[hashCode].value = value;
+
+        if (!Bucket[hashCode]) {
+            Bucket[hashCode] = {
+                key: key,
+                value: value
+            };
+        }
+
+        if (Bucket[hashCode].key === key) {
+            Bucket[hashCode].value = value;
+        } if (Bucket[hashCode] !== null && Bucket[hashCode].key !== key) {
+            if (Bucket[hashCode].nextNode) {
+                let temp = Bucket[hashCode].nextNode;
+                temp.nextNode = new Node()
+            }
+        } 
+        
+    }
+
     return {
-        Hash
+        Hash,
+        Set
     }
 }
 
 const list = HashMap();
 console.log(list.Hash("kojfi"))
 console.log(list.Hash("jKofi"))
+
