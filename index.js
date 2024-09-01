83c3a0b86dcaa89f6ff94b717921fe596e6592cf
Nortey-Micheal <norteymicheal91@gmail.com>
2024-09-01 09:16:03 -0700

    "Refactored HashMap implementation, added get, has, remove methods, and removed index1.js file."

--- index.js
+++ index.js
@@ -1,5 +1,161 @@
-function Node (value = null,next = null) {
+function HashMap () {
+    let Buckets = []
+    let BucketSize = 16;
+    Buckets.length = BucketSize
+
+
+
+    const Hash = (key) => {
+        let hashCode = 0;
+      
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % 16;
+        }
+        console.log(hashCode)
+
+        return hashCode;
+    }
+
+    const set = (key,value) => {
+        let hashCode = Hash(key);
+
+        if (hashCode < 0 || hashCode >= Buckets.length) {
+            throw new Error("Trying to access index out of bound");
+        }
+
+        if (!Buckets[hashCode]) {
+            let list = new LinkedList();
+            list.nextNode = new Node(value,key)
+            Buckets[hashCode] = list
+        } else {
+            let temp = Buckets[hashCode].nextNode;
+            while (temp.nextNode !== null && temp.key !== key) {
+                temp = temp.nextNode;
+            }
+            if (temp.nextNode === null) {
+                temp.nextNode = new Node(value, key)
+            } else {
+                if (temp.key === key) {
+                    temp.value = value;
+                } else if (temp.key !== key) {
+                    temp.nextNode = new Node(value,key)
+                }
+            }
+        }
+    }
+
+    const entries = () => {
+        const Entries = [];
+        
+        for(index in Buckets) {
+            if(Buckets[index]) {
+                let newObj = {};
+                if (Buckets[index].nextNode) {
+                    let temp = Buckets[index].nextNode;
+                    let newObj1 = {key: `${temp.key}`,value: `${temp.value}`};
+                    Entries.push(newObj1)
+                    while (temp.nextNode !== null) {
+                        newObj = {key: `${temp.nextNode.key}`,value: `${temp.nextNode.value}`}
+                        Entries.push(newObj) ;
+                        temp = temp.nextNode;
+                    }
+                    
+                } else {
+                    if (Buckets[index].key === undefined) {
+                        continue;
+                    } else {
+                        newObj = {key: `${Buckets[index].key}`,value: `${Buckets[index].value}`};
+                        Entries.push(newObj)
+                    }
+                }
+            }
+        }
+
+        return Entries
+    }
+
+    const get = (key) => {
+        let hashCode = Hash(key);
+        let value;
+        if (Buckets[hashCode]) {
+            let temp = Buckets[hashCode];
+            while (temp !== null) {
+                if (temp.key === key) {
+                    value = temp.value;
+                    break;
+                } else {
+                    value = null;
+                    temp = temp.nextNode;
+                }
+            }
+
+        } else {
+            value = null;
+        }
+
+        return value;
+    }
+
+    const has = (key) => {
+        let hashCode = Hash(key)
+        if (Buckets[hashCode]) {
+            let temp = Buckets[hashCode];
+            while (temp !== null) {
+                if (temp.key === key) {
+                    return true;
+                } else {
+                    temp = temp.nextNode;
+                }
+            }
+            
+            if (temp === null) return false;
+
+        } else {
+            return false;
+        }
+    }
+
+    const remove = (key) => {
+        let hashCode = Hash(key);
+        if (Buckets[hashCode].key === key) {
+            let newNode = Buckets[hashCode].nextNode
+            Buckets[hashCode] = newNode;
+            return true;
+        } else {
+            let temp = Buckets[hashCode];
+            while (temp.nextNode !== null) {
+                if (temp.nextNode.key === key) {
+                    let newNode = temp.nextNode.nextNode;
+                    temp.nextNode = newNode;
+                    return true;
+                } else {
+                    temp = temp.nextNode;
+                }
+            }
+            return false;
+        }
+    }
+
+    const length = () => {
+        Buckets.forEach(item, () => {
+            
+        })
+    }
+
+    return {
+        Hash,
+        set,
+        get,
+        has,
+        remove,
+        entries
+    }
+}
+
+function Node (value = null,key = null,next = null) {
     this.value = value;
+    this.key = key;
     this.nextNode = next;
 }
 
@@ -17,54 +173,27 @@ const LinkedList = function () {
     }
 
     return {
-        append
+        append,
     }
 }
 
-function HashMap () {
-    let Bucket = [];
-    const Hash = (key) => {
-        let hashCode = 0;
-      
-        const primeNumber = 31;
-        for (let i = 0; i < key.length; i++) {
-          hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % 16;
-        }
-     
-        return hashCode;
-    }
+const test = HashMap()
 
-    const Set = (key,value) => {
-        let hashCode = Hash(key);
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
 
-        // Bucket[hashCode].key = key;
-        // Bucket[hashCode].value = value;
-
-        if (!Bucket[hashCode]) {
-            Bucket[hashCode] = {
-                key: key,
-                value: value
-            };
-        }
-
-        if (Bucket[hashCode].key === key) {
-            Bucket[hashCode].value = value;
-        } if (Bucket[hashCode] !== null && Bucket[hashCode].key !== key) {
-            if (Bucket[hashCode].nextNode) {
-                let temp = Bucket[hashCode].nextNode;
-                temp.nextNode = new Node()
-            }
-        } 
-        
-    }
-
-    return {
-        Hash,
-        Set
-    }
-}
-
-const list = HashMap();
-console.log(list.Hash("kojfi"))
-console.log(list.Hash("jKofi"))
-
+console.log(test.entries())
+console.log(test.get('dog'))
+console.log(test.has('monkey'))
+console.log(test.remove('kite'))
+console.log(test.entries())
